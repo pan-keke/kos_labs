b96e4edf47ea875d9a70cff61c8cf356b5d71443
pan-keke <andrey080604.1251@gmail.com>
2025-05-19 12:01:49 +0300

    gh

--- bank_customer_outflow/prediction_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('predict/', views.predict, name='predict'),
-    path('api/predict/', views.predict_api, name='predict_api'),
-    path('train/', views.train_model, name='train_model'),
-]
